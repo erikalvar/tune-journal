a9ab23d0915c1f9914e8bcdef0645c2797ad66b6
erikalvar <erikalvarbass@gmail.com>
2021-01-06 12:46:33 -0500

    adds current user to all song actions

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,6 +1,8 @@
 class Api::SongsController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    @songs = Song.all.where(:user_id == current_user.id)
+    @songs = Song.where(user_id: current_user.id).order(:title)
     render "index.json.jb"
   end
 
@@ -25,29 +27,41 @@ class Api::SongsController < ApplicationController
 
   def show
     @song = Song.find(params[:id])
-    render "show.json.jb"
+    if @song.user_id == current_user.id
+      render "show.json.jb"
+    else
+      render json: {}, status: :unauthorized
+    end
   end
 
   def update
     @song = Song.find(params[:id])
-    @song.title = params[:title] || @song.title
-    @song.composer = params[:composer] || @song.composer
-    @song.key = params[:key] || @song.key
-    @song.mode = params[:mode] || @song.mode
-    @song.tempo = params[:tempo] || @song.tempo
-    @song.duration = params[:duration] || @song.duration
-    @song.status = params[:status] || @song.status
-    @song.notes = params[:notes] || @song.notes
-    if @song.save
-      render "show.json.jb"
+    if @song.user_id == current_user.id
+      @song.title = params[:title] || @song.title
+      @song.composer = params[:composer] || @song.composer
+      @song.key = params[:key] || @song.key
+      @song.mode = params[:mode] || @song.mode
+      @song.tempo = params[:tempo] || @song.tempo
+      @song.duration = params[:duration] || @song.duration
+      @song.status = params[:status] || @song.status
+      @song.notes = params[:notes] || @song.notes
+      if @song.save
+        render "show.json.jb"
+      else
+        render json: { errors: @event.errors.full_messages }, status: :unprocessable_entity
+      end
     else
-      render json: { errors: @event.errors.full_messages }, status: :unprocessable_entity
+      render json: {}, status: :unauthorized
     end
   end
 
   def destroy
     @song = Song.find(params[:id])
-    @song.destroy
-    render json: { message: "song successfully deleted." }
+    if @song.user_id == current_user.id
+      @song.destroy
+      render json: { message: "song successfully deleted." }
+    else
+      render json: {}, status: :unauthorized
+    end
   end
 end
